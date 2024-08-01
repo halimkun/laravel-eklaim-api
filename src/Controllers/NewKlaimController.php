155e3675acbc6976a401f8932be420f5ed86a2d8
Faisal Halim <ffaisalhalim@gmail.com>
2024-08-01 09:54:06 +0700

    :sparkles: feat (new_claim): Add API endpoint for creating new klaim
    This code change adds a new API endpoint in the NewKlaimController to handle requests for creating a new klaim using the E-KLAIM API. The request data is validated to ensure all required information is present and in the correct format. Once validated, the data is packaged in JSON format and sent to the E-KLAIM API using the EklaimService. This change improves the functionality of the application by allowing users to create new klaims through the API.

--- src/Controllers/NewKlaimController.php
+++ src/Controllers/NewKlaimController.php
@@ -6,19 +6,44 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use FaisalHalim\LaravelEklaimApi\Services\EklaimService;
 
+/**
+ * Menangani permintaan untuk membuat klaim baru dengan API E-KLAIM.
+ * 
+ * Validasi dilakukan pada data permintaan untuk memastikan semua
+ * informasi yang diperlukan ada dan dalam format yang benar. Setelah
+ * validasi berhasil, data dikemas dalam format JSON dan dikirim ke
+ * API E-KLAIM menggunakan layanan EklaimService.
+ *
+ * @param \Illuminate\Http\Request $request
+ * @return \Illuminate\Http\Response
+ */
 class NewKlaimController extends Controller
 {
-    protected $eklaimService;
-
-    public function __construct(EklaimService $eklaimService)
-    {
-        $this->eklaimService = $eklaimService;
-    }
-
     public function handle(Request $request)
     {
+        $request->validate([
+            "nomor_kartu"   => "required",
+            "nomor_sep"     => "required",
+            "nomor_rm"      => "required",
+            "nama_pasien"   => "required",
+            "tgl_lahir"     => "required|date_format:Y-m-d H:i:s",
+            "gender"        => "required|in:1,2",                       // 1: Laki-laki, 2: Perempuan
+        ]);
 
+        $json = [
+            "metadata" => [
+                "method"        => 'new_claim'
+            ],
+            "data" => [
+                "nomor_kartu"   => $request->nomor_kartu,
+                "nomor_sep"     => $request->nomor_sep,
+                "nomor_rm"      => $request->nomor_rm,
+                "nama_pasien"   => $request->nama_pasien,
+                "tgl_lahir"     => $request->tgl_lahir,
+                "gender"        => $request->gender
+            ]
+        ];
+
+        return EklaimService::post($json);
     }
-
-
 }
