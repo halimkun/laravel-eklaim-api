fc5f6c351819896226be34ad210ccefffeb9714f
Faisal Halim <ffaisalhalim@gmail.com>
2024-08-01 09:53:18 +0700

    :sparkles: feat: Add initial controllers and configuration files

--- src/Config/eklaim.php
+++ src/Config/eklaim.php
@@ -1,6 +1,49 @@
 <?php
 
 return [
-    'api_url'    => env('EKLAIM_API_URL'),
+    /**
+     * URL dasar untuk API E-KLAIM. Nilai ini diambil dari variabel lingkungan
+     * EKLAIM_API_URL yang diatur di file .env. URL ini digunakan untuk membuat
+     * permintaan ke API E-KLAIM.
+     *
+     * @var string
+     */
+    'api_url' => env('EKLAIM_API_URL'),
+
+    /**
+     * Kunci rahasia yang digunakan untuk enkripsi dan dekripsi data.
+     * Nilai ini diambil dari variabel lingkungan EKLAIM_SECRET_KEY yang diatur
+     * di file .env. Kunci ini harus dalam format hexadecimal dan panjang 256-bit.
+     *
+     * @var string
+     */
     'secret_key' => env('EKLAIM_SECRET_KEY'),
+
+    /**
+     * Menentukan apakah respons dari API harus didekripsi.
+     * Jika diatur ke true, data respons yang diterima dari API akan didekripsi
+     * menggunakan kunci rahasia yang diberikan. Secara default, dekripsi dinonaktifkan.
+     *
+     * @var bool
+     */
+    'decrypt_response' => true,
+
+    /**
+     * Menentukan apakah respons dari API harus diubah secara otomatis ke format JSON.
+     * Jika diatur ke true, respons yang diterima akan secara otomatis dikemas dalam
+     * format JSON sebelum dikembalikan. Berguna untuk menjaga konsistensi format respons.
+     *
+     * @var bool
+     */
+    'json_response' => true,
+
+    /**
+     * Nama saluran log yang digunakan untuk mencatat kesalahan yang terjadi
+     * selama interaksi dengan API E-KLAIM. Secara default, kesalahan akan dicatat
+     * ke saluran log 'default'. Anda dapat mengubah ini untuk mencatat ke saluran log
+     * yang berbeda yang telah dikonfigurasi di file konfigurasi logging.
+     *
+     * @var string
+     */
+    'log_channel' => 'default',
 ];
--- src/Services/EklaimService.php
+++ src/Services/EklaimService.php
@@ -4,39 +4,111 @@ namespace FaisalHalim\LaravelEklaimApi\Services;
 
 use FaisalHalim\LaravelEklaimApi\Helpers\EKlaimCrypt;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Response;
 
 class EklaimService
 {
-    protected $apiUrl;
-    protected $secretKey;
-    protected $encryptor;
+    /**
+     * URL API yang digunakan untuk mengirimkan permintaan.
+     * 
+     * @var string
+     */
+    protected static $apiUrl;
 
-    public function __construct($apiUrl, $secretKey, EKlaimCrypt $encryptor)
+    /**
+     * Kunci rahasia yang digunakan untuk enkripsi dan dekripsi.
+     * 
+     * @var string
+     */
+    protected static $secretKey;
+
+    /**
+     * Objek untuk melakukan enkripsi dan dekripsi.
+     * 
+     * @var EKlaimCrypt
+     */
+    protected static $encryptor;
+
+    /**
+     * Mengkonfigurasi layanan EklaimService dengan URL API, kunci rahasia, dan objek enkripsi.
+     * 
+     * @param string $apiUrl
+     * @param string $secretKey
+     * @param EKlaimCrypt $encryptor
+     * @return void
+     */
+    public static function configure($apiUrl, $secretKey, EKlaimCrypt $encryptor)
     {
-        $this->apiUrl    = $apiUrl;
-        $this->secretKey = $secretKey;
-        $this->encryptor = $encryptor;
+        self::$apiUrl = $apiUrl;
+        self::$secretKey = $secretKey;
+        self::$encryptor = $encryptor;
     }
 
-    public function post($data)
+    /**
+     * Mengirim data yang dienkripsi ke API eklaim dan mengembalikan respons yang diproses.
+     * 
+     * @param array $data
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function post($data)
     {
-        $encryptedData = $this->encryptor::encrypt(json_encode($data), $this->secretKey);
-        
-        // ensure the data is correctly formatted as a single-line string
+        $encryptedData = self::$encryptor::encrypt(json_encode($data), self::$secretKey);
         $encryptedData = trim(preg_replace('/\s+/', '', $encryptedData));
 
         try {
             $response = Http::withHeaders([
                 'Content-Type' => 'application/x-www-form-urlencoded',
-            ])->post($this->apiUrl, [ $encryptedData, ]);
+            ])->post(self::$apiUrl, [$encryptedData]);
         } catch (\Throwable $th) {
-            throw new \Exception("Failed to submit data to eklaim API: " . $th->getMessage());
+            \Log::channel(config('eklaim.log_channel', 'default'))->error("E-KLAIM API Error", [
+                'data' => $data,
+                'response' => $th->getMessage(),
+            ]);
+
+            if (config('eklaim.json_response')) {
+                return Response::json([
+                    'metadata' => [
+                        'code'    => 500,
+                        'message' => 'Internal Server Error',
+                    ]
+                ], 500);
+            }
+
+            throw new \Exception("Internal Server Error");
         }
 
-        $first = strpos($response, "\n") + 1;
-        $last  = strrpos($response, "\n") - 1;
-        $data  = substr($response, $first, strlen($response) - $first - $last);
+        $data = self::extractResponseData($response);
 
-        return $data;
+        if (config('eklaim.decrypt_response', false)) {
+            $data = self::$encryptor::decrypt($data, self::$secretKey);
+        }
+
+        return config('eklaim.json_response', false) ? Response::json(json_decode($data), self::getResponseStatusCode($data)) : $data;
+    }
+
+    /**
+     * Mengekstrak data dari respons API dengan memotong bagian yang tidak diperlukan.
+     * 
+     * @param string $response
+     * @return string
+     */
+    protected static function extractResponseData($response)
+    {
+        $first = strpos($response, "\n") + 1;
+        $last = strrpos($response, "\n") - 1;
+        return substr($response, $first, strlen($response) - $first - $last);
+    }
+
+    /**
+     * Mendapatkan kode status dari respons yang telah didekripsi.
+     * 
+     * @param string $data
+     * @return int
+     */
+    protected static function getResponseStatusCode($data)
+    {
+        $jsonData = json_decode($data);
+        return $jsonData->metadata->code ?? 200;
     }
 }
