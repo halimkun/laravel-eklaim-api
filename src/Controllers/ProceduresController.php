b74f34edabb9264ae314f10ba67eb60485658b28
Faisal Halim <ffaisalhalim@gmail.com>
2024-08-01 10:47:15 +0700

    :sparkles: feat: Add search functionality to ProceduresController and ProceduresInaController

--- src/Controllers/ProceduresController.php
+++ src/Controllers/ProceduresController.php
@@ -2,10 +2,27 @@
 
 namespace FaisalHalim\LaravelEklaimApi\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use FaisalHalim\LaravelEklaimApi\Services\EklaimService;
 
 class ProceduresController extends Controller
 {
-    //
+    public function handle(Request $request)
+    {
+        $request->validate([
+            'search.value' => 'required|string',
+        ]);
+
+        $json = [
+            "metadata" => [
+                "method" => 'search_procedures'
+            ],
+            "data" => [
+                "keyword" => $request->input('search.value')
+            ]
+        ];
+
+        return EklaimService::post($json);
+    }
 }
