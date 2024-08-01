d727414df4b733f640510d6448a612b51347c9d3
Faisal Halim <ffaisalhalim@gmail.com>
2024-08-01 10:46:49 +0700

    :sparkles: feat: Add search functionality to DiagnosisController and DiagnosisInaController
    This code change adds search functionality to the DiagnosisController and DiagnosisInaController. The `handle` method in both controllers now validates the search value from the request and sends a JSON payload to the EklaimService for searching diagnosis information. This enhancement improves the functionality of the application by allowing users to search for diagnosis data through the API.

--- src/Controllers/DiagnosisInaController.php
+++ src/Controllers/DiagnosisInaController.php
@@ -2,10 +2,27 @@
 
 namespace FaisalHalim\LaravelEklaimApi\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use FaisalHalim\LaravelEklaimApi\Services\EklaimService;
 
 class DiagnosisInaController extends Controller
 {
-    //
+    public function handle(Request $request)
+    {
+        $request->validate([
+            'search.value' => 'required|string',
+        ]);
+
+        $json = [
+            "metadata" => [
+                "method" => 'search_diagnosis_inagrouper'
+            ],
+            "data" => [
+                "keyword" => $request->input('search.value')
+            ]
+        ];
+
+        return EklaimService::post($json);
+    }
 }
