bab110aeff1c4bf991ba04738fae5499040ecc16
Faisal Halim <ffaisalhalim@gmail.com>
2024-08-01 09:54:44 +0700

    :sparkles: feat (patient) : Add update and delete methods to PatientController
    This code change adds the update and delete methods to the PatientController. The update method handles the updating of patient information by validating the request data and sending it to the E-KLAIM API. The delete method handles the deletion of a patient by sending a request to the E-KLAIM API with the patient's record number and coder information. These methods enhance the functionality of the application by allowing users to update and delete patient data through the API.

--- src/Controllers/PatientController.php
+++ src/Controllers/PatientController.php
@@ -2,10 +2,50 @@
 
 namespace FaisalHalim\LaravelEklaimApi\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use FaisalHalim\LaravelEklaimApi\Services\EklaimService;
 
 class PatientController extends Controller
 {
-    //
+    public function update($no_rekam_medis, Request $request)
+    {
+        $request->validate([
+            "nomor_kartu"   => "required",
+            "nama_pasien"   => "required",
+            "tgl_lahir"     => "required|date_format:Y-m-d H:i:s",
+            "gender"        => "required|in:1,2",                       // 1: Laki-laki, 2: Perempuan
+        ]);
+
+        $json = [
+            "metadata" => [
+                "method"        => 'update_patient',
+                "nomor_rm"      => $no_rekam_medis
+            ],
+            "data" => [
+                "nomor_kartu"   => $request->nomor_kartu,
+                "nomor_rm"      => $no_rekam_medis,
+                "nama_pasien"   => $request->nama_pasien,
+                "tgl_lahir"     => $request->tgl_lahir,
+                "gender"        => $request->gender
+            ]
+        ];
+
+        return EklaimService::post($json);
+    }
+
+    public function delete($no_rekam_medis, $coder)
+    {
+        $json = [
+            "metadata" => [
+                "method"        => 'delete_patient'
+            ],
+            "data" => [
+                "nomor_rm"      => $no_rekam_medis,
+                "coder_nik"     => $coder
+            ]
+        ];
+
+        return EklaimService::post($json);
+    }
 }
