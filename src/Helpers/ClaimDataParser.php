b53b6cf3897a5896b662d2a46a2fa19b06452164
Faisal Halim <ffaisalhalim@gmail.com>
2024-08-06 14:25:30 +0700

    :sparkles: feat: Add ClaimDataParser class for parsing claim data
    This code change adds the ClaimDataParser class to the FaisalHalim\LaravelEklaimApi\Helpers namespace. The ClaimDataParser class provides a static method, parse, which takes a Request object as input and parses the data from the request into an array. The class handles optional data, kelar rawat data, upgrade class data, ICU data, diagnosis and procedure data, tarif RS data, pelayanan darah data, and persalinan data. This addition improves the codebase by encapsulating the logic for parsing claim data into a dedicated class, enhancing code organization and maintainability.

---
+++ src/Helpers/ClaimDataParser.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace FaisalHalim\LaravelEklaimApi\Helpers;
+
+use Illuminate\Http\Request;
+
+class ClaimDataParser
+{
+    public static function parse(Request $request)
+    {
+        $data = [];
+
+        self::parseOptionalData($request, $data);
+        self::handleKelarRawatData($request, $data);
+        self::handleUpgradeClassData($request, $data);
+        self::handleIcuData($request, $data);
+        self::handleDiagnosisAndProcedureData($request, $data);
+        self::handleTarifRsData($request, $data);
+        self::handlePelayananDarahData($request, $data);
+        self::handlePersalinanData($request, $data);
+
+        return $data;
+    }
+
+    protected static function parseOptionalData(Request $request, array &$data)
+    {
+        $optionalFields = [
+            'tgl_masuk', 'tgl_pulang', 'cara_masuk', 'jenis_rawat',
+            'adl_sub_acute', 'adl_chronic', 'birth_weight', 'sistole', 
+            'diastole',  'discharge_status', 'terapi_konvalesen', 
+            'tarif_poli_eks', 'nama_dokter', 'kode_tarif'
+        ];
+
+        foreach ($optionalFields as $field) {
+            if ($request->has($field)) {
+                $data[$field] = $request->input($field);
+            }
+        }
+    }
+
+    protected static function handleKelarRawatData(Request $request, array &$data)
+    {
+        if ($request->has('jenis_rawat')) {
+            if ($request->jenis_rawat == 2) {
+                $request->validate([
+                    "kelas_rawat" => "in:1,3"
+                ]);
+            }
+        }
+
+        $data['kelas_rawat'] = $request->kelas_rawat;
+    }
+
+    protected static function handleUpgradeClassData(Request $request, array &$data)
+    {
+        $upgradeFields = ['upgrade_class_ind', 'upgrade_class_class', 'upgrade_class_los', 'add_payment_pct', 'upgrade_class_payor'];
+        if ($request->hasAny($upgradeFields)) {
+            $validationRules = array_fill_keys($upgradeFields, 'required');
+            $request->validate($validationRules);
+
+            foreach ($upgradeFields as $field) {
+                $data[$field] = $request->input($field);
+            }
+        }
+    }
+
+    protected static function handleIcuData(Request $request, array &$data)
+    {
+        if ($request->icu_indikator == 1) {
+            $request->validate([
+                "icu_los" => "required|numeric",
+            ]);
+
+            $data['icu_los'] = $request->icu_los;
+
+            // Handle ventilator data
+            $ventilatorFields = ['use_ind', 'start_dttm', 'stop_dttm'];
+            if ($request->hasAny($ventilatorFields)) {
+                $validationRules = array_fill_keys($ventilatorFields, 'required');
+                $request->validate($validationRules);
+
+                foreach ($ventilatorFields as $field) {
+                    $data['ventilator'][$field] = $request->input($field);
+                }
+            }
+
+            // Calculate ventilator hours
+            if (isset($data['ventilator']['start_dttm']) && isset($data['ventilator']['stop_dttm'])) {
+                $start = new \DateTime($data['ventilator']['start_dttm']);
+                $stop = new \DateTime($data['ventilator']['stop_dttm']);
+                $diff = $start->diff($stop);
+                $data['ventilator_hour'] = ceil($diff->h + ($diff->i / 60));
+            }
+        } else {
+            $data['icu_indikator'] = 0;
+            $data['icu_los'] = 0;
+        }
+    }
+
+    protected static function handleDiagnosisAndProcedureData(Request $request, array &$data)
+    {
+        $procedureFields = ['diagnosa', 'diagnosa_inagrouper', 'procedure', 'procedure_inagrouper'];
+        foreach ($procedureFields as $field) {
+            if ($request->has($field)) {
+                $imploded = implode('#', $request->input($field));
+                $data[$field] = $imploded;
+            }
+        }
+    }
+
+    protected static function handleTarifRsData(Request $request, array &$data)
+    {
+        $tarifRsFields = ['prosedur_non_bedah', 'prosedur_bedah', 'konsultasi', 'tenaga_ahli', 'keperawatan', 'penunjang', 'radiologi', 'laboratorium', 'pelayanan_darah', 'rehabilitasi', 'kamar', 'rawat_intensif', 'obat', 'obat_kronis', 'obat_kemoterapi', 'alkes', 'bmhp', 'sewa_alat'];
+        foreach ($tarifRsFields as $trs) {
+            if ($request->has($trs)) {
+                $data['tarif_rs'][$trs] = $request->input($trs);
+            }
+        }
+    }
+
+    protected static function handlePelayananDarahData(Request $request, array &$data)
+    {
+        if ($request->has('pelayanan_darah') && $request->pelayanan_darah > 0) {
+            $request->validate([
+                'kantong_darah' => 'required|numeric',
+            ]);
+
+            $data['kantong_darah'] = $request->kantong_darah;
+        }
+    }
+
+    protected static function handlePersalinanData(Request $request, array &$data)
+    {
+        $persalinanFields = ['usia_kehamilan', 'gravida', 'partus', 'abortus', 'onset_kontraksi'];
+        foreach ($persalinanFields as $pf) {
+            if ($request->has($pf)) {
+                $data['persalinan'][$pf] = $request->input($pf);
+            }
+        }
+    }
+}
